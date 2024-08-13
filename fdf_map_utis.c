9a98548aae551d8126c023231174cb5f3a7e2952
szb <chamaselion@gmail.com>
2024-08-13 11:38:07 +0200

    Added printf for debugging larger maps segfault. Added error handling and free in case of error. Changed count integers function iteration for correct results.

--- fdf_map_utis.c
+++ fdf_map_utis.c
@@ -63,7 +63,7 @@ int count_integers_in_string(char *str)
     int length = strlen(str);
     int inNumber = 0;
 
-    for (int i = 0; i <= length; i++)
+    for (int i = 0; i <= length - 1; i++)
     {
         if (!isspace(str[i]) && str[i] != '\0')
         {
@@ -85,26 +85,82 @@ int count_integers_in_string(char *str)
 void fill_map_lines(t_Map *map, char *line)
 {
     int length;
+    printf("before string to arr\n");
     int *intArray = string_to_int_array(line, &length);
+    printf("after string to arr\n");
     if (intArray == NULL)
         return;
 
-    map->values = (int **)ft_realloc(map->values, sizeof(int *) * (map->height + 1));
+    // Allocate memory for the new row
+    printf("before realloc\n");
+    int **new_values = (int **)ft_realloc(map->values, sizeof(int *) * (map->height + 1));
+    if (new_values == NULL)
+    {
+        free(intArray); // Free the allocated intArray if realloc fails
+        perror("Error reallocating memory");
+        return;
+    }
+    printf("after realloc\n");
+
+    map->values = new_values;
     map->values[map->height] = intArray;
     map->width = length;
     map->height++;
+    printf("after updating map\n");
 }
 
-void    map_filling(t_Map *map)
+int map_filling(t_Map *map)
 {
     int fd;
     char *line;
 
-    fd = open_file("test.fdf");
+    printf("before openfile\n");
+    fd = open_file("test_maps/42.fdf");
+    printf("openfile\n");
+    if (fd <= 1)
+    {
+        perror("Error opening file");
+        return (-1);
+    }
+
+    printf("before gnl\n");
     while ((line = get_next_line(fd)) != NULL)
     {
         fill_map_lines(map, line);
+        if (map->height > 0 && map->values != NULL && map->values[0] != NULL)
+        {
+            printf("gnl:%i\n", map->values[0][0]);
+        }
         free(line);
     }
+
+    printf("after gnl\n");
+    if (map->height == 0)
+    {
+        close(fd);
+        perror("Error reading file");
+        return (-1);
+    }
+
     close(fd);
+    return (0);
+}
+
+void print_map(t_Map *map)
+{
+    if (map == NULL || map->values == NULL)
+    {
+        printf("Map is empty or not initialized.\n");
+        return;
+    }
+
+    printf("Map (width: %d, height: %d):\n", map->width, map->height);
+    for (int i = 0; i < map->height; i++)
+    {
+        for (int j = 0; j < map->width; j++)
+        {
+            printf("%d ", map->values[i][j]);
+        }
+        printf("\n");
+    }
 }

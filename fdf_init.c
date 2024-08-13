eca0c452c449efd7b4f7de11dcd1191fd8d0f4be
szb <chamaselion@gmail.com>
2024-08-13 11:33:58 +0200

    Added changes to the init color function for changing color based on the y value, Also added more complex init image size to help with centering the image. Needs furthe improvement

--- fdf_init.c
+++ fdf_init.c
@@ -1,22 +1,42 @@
 #include "fdf.h"
 
-void init_window_size(t_ImageSize *Window, t_Map *map)
+void init_window_size(t_DrawData *drawData, t_Map *map)
 {
-    Window->x = 1000;
-    Window->y = 1000;
+    // Get the window size to the default values
+   // mlx_get_screen_size(drawData->mlx, &drawData->windowSize.x, &drawData->windowSize.y);
+    drawData->windowSize.x = 1200;
+    drawData->windowSize.y = 1200;
+
 }
 
-void init_image_size(t_ImageSize *Image, t_Map *map)
+void init_image_size(t_DrawData *drawData, t_Map *map)
 {
-    Image->x = 800;
-    Image->y = 800;
+    drawData->imageSize.x = 800; // Assuming the image size is 800x800
+    drawData->imageSize.y = 800;
+
+    // Calculate the center position of the window
+    int window_center_x = drawData->windowSize.x / 2;
+    int window_center_y = drawData->windowSize.y / 2;
+
+    // Calculate the center position of the image
+    int image_center_x = drawData->imageSize.x / 2;
+    int image_center_y = drawData->imageSize.y / 2;
+
+    // Adjust the image position to center it in the window
+    drawData->imagePosition.x = window_center_x - image_center_x;
+    drawData->imagePosition.y = window_center_y - image_center_y;
 }
 
-int init_color()
+int init_color(int z, int min_z, int max_z)
 {
-    int clr;
+    // Normalize the height value to a range between 0 and 1
+    float normalized = (float)(z - min_z) / (max_z - min_z);
 
-    clr = 0x00FF0000;
+    // Map the normalized value to a color gradient (e.g., blue to red)
+    int red = (int)(normalized * 255);
+    int green = 0;
+    int blue = (int)((1 - normalized) * 255);
 
-    return clr;
+    // Combine the RGB values into a single integer
+    return (red << 16) | (green << 8) | blue;
 }

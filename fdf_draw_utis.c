b84e462a17a902d327770ff71cfef35b915afb1c
szb <chamaselion@gmail.com>
2024-08-23 14:45:44 +0200

    Norminettize

--- fdf_draw_utis.c
+++ fdf_draw_utis.c
@@ -1,93 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_draw_utis.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 13:11:38 by bszikora          #+#    #+#             */
+/*   Updated: 2024/08/23 14:28:38 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-void put_pixel_to_image(void *image, int x, int y, int color)
+void	put_pixel_to_image(void *image, int x, int y, int color)
 {
-    char *buffer;
-    int pixel_bits, line_bytes, endian;
-    buffer = mlx_get_data_addr(image, &pixel_bits, &line_bytes, &endian);
-    int pos = (y * line_bytes) + (x * (pixel_bits / 8));
-    *(int *)(buffer + pos) = color;
+	char	*buffer;
+	int		pos;
+	int		pixel_bits;
+	int		line_bytes;
+	int		endian;
+
+	buffer = mlx_get_data_addr(image, &pixel_bits, &line_bytes, &endian);
+	pos = (y * line_bytes) + (x * (pixel_bits / 8));
+	*(int *)(buffer + pos) = color;
 }
 
-void draw_line(t_LineData *lineData, t_DrawData *drawData)
+void	draw_line(t_linedata *linedata, t_drawdata *drawdata)
 {
-    int image_width;
-    int image_height;
-    
-    image_width = drawData->windowSize.x;
-    image_height = drawData->windowSize.y;
+	int	image_width;
+	int	image_height;
 
-
-    if (lineData->x0 < 0 || lineData->x0 >= image_width || lineData->y0 < 0 || lineData->y0 >= image_height ||
-        lineData->x1 < 0 || lineData->x1 >= image_width || lineData->y1 < 0 || lineData->y1 >= image_height)
-    {
-        printf("Coordinates out of bounds: (%d, %d) to (%d, %d)\n", lineData->x0, lineData->y0, lineData->x1, lineData->y1);
-        return;
-    }
-
-    lineData->dx = abs(lineData->x1 - lineData->x0);
-    lineData->dy = abs(lineData->y1 - lineData->y0);
-
-    if (lineData->x0 < lineData->x1)
-        lineData->sx = 1;
-    else
-        lineData->sx = -1;
-    if (lineData->y0 < lineData->y1)
-        lineData->sy = 1;
-    else
-        lineData->sy = -1;
-
-    if (lineData->dx > lineData->dy)
-        lineData->err = lineData->dx;
-    else
-        lineData->err = -lineData->dy;
-    lineData->err /= 2;
-
-    while (1)
-    {
-        put_pixel_to_image(drawData->image, lineData->x0, lineData->y0, lineData->color);
-        if (lineData->x0 == lineData->x1 && lineData->y0 == lineData->y1)
-            break;
-        lineData->e2 = lineData->err;
-        if (lineData->e2 > -lineData->dx)
-        {
-            lineData->err -= lineData->dy;
-            lineData->x0 += lineData->sx;
-        }
-        if (lineData->e2 < lineData->dy)
-        {
-            lineData->err += lineData->dx;
-            lineData->y0 += lineData->sy;
-        }
-    }
+	image_width = drawdata->windowsize.x;
+	image_height = drawdata->windowsize.y;
+	if (linedata->x0 < 0 || linedata->x0 >= image_width || linedata->y0 < 0
+		|| linedata->y0 >= image_height || linedata->x1 < 0
+		|| linedata->x1 >= image_width || linedata->y1 < 0
+		|| linedata->y1 >= image_height)
+	{
+		printf("coordinates out of bounds: (%d, %d) to (%d, %d)\n",
+			linedata->x0, linedata->y0, linedata->x1, linedata->y1);
+		return ;
+	}
+	linedata->dx = abs(linedata->x1 - linedata->x0);
+	linedata->dy = abs(linedata->y1 - linedata->y0);
+	if (linedata->x0 < linedata->x1)
+		linedata->sx = 1;
+	else
+		linedata->sx = -1;
+	if (linedata->y0 < linedata->y1)
+		linedata->sy = 1;
+	else
+		linedata->sy = -1;
+	if (linedata->dx > linedata->dy)
+		linedata->err = linedata->dx;
+	else
+		linedata->err = -linedata->dy;
+	linedata->err /= 2;
+	while (1)
+	{
+		put_pixel_to_image(drawdata->image, linedata->x0, linedata->y0,
+			linedata->color);
+		if (linedata->x0 == linedata->x1 && linedata->y0 == linedata->y1)
+			break ;
+		linedata->e2 = linedata->err;
+		if (linedata->e2 > -linedata->dx)
+		{
+			linedata->err -= linedata->dy;
+			linedata->x0 += linedata->sx;
+		}
+		if (linedata->e2 < linedata->dy)
+		{
+			linedata->err += linedata->dx;
+			linedata->y0 += linedata->sy;
+		}
+	}
 }
 
-int close_window(t_DrawData *drawData)
+int	close_window(t_drawdata *drawdata)
 {
-    if (drawData->window != NULL)
-    {
-        mlx_destroy_window(drawData->mlx, drawData->window);
-        drawData->window = NULL;
-    }
-    free_draw_data(drawData);
-    free_map(drawData->map);
-    exit(0);
-    return 0;
+	if (drawdata->window != NULL)
+	{
+		mlx_destroy_window(drawdata->mlx, drawdata->window);
+		drawdata->window = NULL;
+	}
+	free_draw_data(drawdata);
+	free_map(drawdata->map);
+	exit(0);
+	return (0);
 }
 
-int key_event_handler(int keycode, t_DrawData *drawData)
+int	key_event_handler(int keycode, t_drawdata *drawdata)
 {
-    if (keycode == 65307) // Escape key
-    {
-        if (drawData->window != NULL)
-        {
-            mlx_destroy_window(drawData->mlx, drawData->window);
-            drawData->window = NULL;
-        }
-        free_draw_data(drawData);
-        free_map(drawData->map);
-        exit(0);
-    }
-    // Handle other keys here
-    return 0;
+	if (keycode == 65307)
+	{
+		if (drawdata->window != NULL)
+		{
+			mlx_destroy_window(drawdata->mlx, drawdata->window);
+			drawdata->window = NULL;
+		}
+		free_draw_data(drawdata);
+		free_map(drawdata->map);
+		exit(0);
+	}
+	return (0);
 }

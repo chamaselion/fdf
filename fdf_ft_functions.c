68be6a6b5a65e016e4364ede79561aad30d49b52
szb <chamaselion@gmail.com>
2024-08-23 14:47:06 +0200

    Norminettize

--- fdf_ft_functions.c
+++ fdf_ft_functions.c
@@ -1,33 +1,36 @@
-#include "fdf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_ft_functions.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 13:14:54 by bszikora          #+#    #+#             */
+/*   Updated: 2024/08/23 13:15:11 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "fdf.h"
 
-void *ft_realloc(void *ptr, size_t old_size, size_t new_size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-    void *new_ptr;
+	void	*new_ptr;
 
-    if (new_size == 0)
-    {
-        free(ptr);
-        return NULL;
-    }
-
-    new_ptr = malloc(new_size);
-    if (new_ptr == NULL)
-        return NULL;
-
-    if (ptr != NULL)
-    {
-        if (old_size < new_size)
-        {
-            memmove(new_ptr, ptr, old_size);
-        }
-        else
-        {
-            memmove(new_ptr, ptr, new_size);
-        }
-        free(ptr);
-    }
-
-    return new_ptr;
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (ptr != NULL)
+	{
+		if (old_size < new_size)
+			memmove(new_ptr, ptr, old_size);
+		else
+			memmove(new_ptr, ptr, new_size);
+		free(ptr);
+	}
+	return (new_ptr);
 }

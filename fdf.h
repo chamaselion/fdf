f62dda8fce393fdbd40d973cb87180258377cd3e
szb <chamaselion@gmail.com>
2024-08-23 14:48:32 +0200

    Norminettize

--- fdf.h
+++ fdf.h
@@ -1,125 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 14:33:47 by bszikora          #+#    #+#             */
+/*   Updated: 2024/08/23 14:44:24 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FDF_H
-#define FDF_H
+# define FDF_H
 
-#include "mlx.h"
-#include "get_next_line/get_next_line.h"
-#include <stdlib.h> // For malloc, free, and atoi
-#include <ctype.h>  // For isspace
-#include <string.h> // For strlen
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <math.h>
-#include <limits.h>
+# include "get_next_line/get_next_line.h"
+# include "mlx.h"
+# include <ctype.h> // For isspace
+# include <fcntl.h>
+# include <limits.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h> // For malloc, free, and atoi
+# include <string.h> // For strlen
+# include <unistd.h>
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
+# ifndef M_PI
+#  define M_PI 3.14159265358979323846
+# endif
 
-    typedef struct
+typedef struct s_imagesize
 {
-    int x;
-    int y;
-    int sx;
-    int sy;
-} t_ImageSize;
+	int			x;
+	int			y;
+	int			sx;
+	int			sy;
+}				t_imagesize;
 
-typedef struct s_LineData {
-    int x0;
-    int y0;
-    int x1;
-    int y1;
-    int color;
-    int dx;
-    int dy;
-    int sx;
-    int sy;
-    int err;
-    int e2;
-} t_LineData;
-
-typedef struct s_DrawMapVars {
-    int max_z;
-    int min_z;
-    float z_scale;
-    int map_center_x;
-    int map_center_y;
-    int offsetX;
-    int offsetY;
-    int x2D;
-    int y2D;
-    int next_x2D;
-    int next_y2D;
-    float scaled_z;
-    float scaled_next_z;
-    float x_iso;
-    float y_iso;
-    float next_x_iso;
-    float next_y_iso;
-} t_DrawMapVars;
-
-typedef struct
+typedef struct s_linedata
 {
-    int width, height;
-    int **values;
-} t_Map;
+	int			x0;
+	int			y0;
+	int			x1;
+	int			y1;
+	int			color;
+	int			dx;
+	int			dy;
+	int			sx;
+	int			sy;
+	int			err;
+	int			e2;
+}				t_linedata;
 
-typedef struct s_Camera {
-    float x, y, z; // Position
-    float pitch, yaw; // Orientation
-} t_Camera;
+typedef struct s_drawmapvars
+{
+	int			max_z;
+	int			min_z;
+	float		z_scale;
+	int			map_center_x;
+	int			map_center_y;
+	int			offsetx;
+	int			offsety;
+	int			x2d;
+	int			y2d;
+	int			next_x2d;
+	int			next_y2d;
+	float		scaled_z;
+	float		scaled_next_z;
+	float		x_iso;
+	float		y_iso;
+	float		next_x_iso;
+	float		next_y_iso;
+}				t_drawmapvars;
 
-typedef struct s_DrawData {
-    void *mlx;
-    void *window;
-    void *image;
-    t_ImageSize windowSize;
-    t_ImageSize imageSize;
-    float scaleX;
-    float scaleY;
-    float scale;
-    int offsetX;
-    int offsetY;
-    int bpp;
-    int line_bytes;
-    int endian;
-    char *original_data;
-    void *scaled_image;
-    int color;
-    t_Map *map;
-} t_DrawData;
+typedef struct s_map
+{
+	int			width;
+	int			height;
+	int			**values;
+}				t_map;
 
-// Draw utilities
-void put_pixel_to_image(void *image, int x, int y, int color);
-void draw_line(t_LineData *lineData, t_DrawData *drawData);
-int close_window(t_DrawData *drawData);
-int key_event_handler(int keycode, t_DrawData *drawData);
+typedef struct s_drawdata
+{
+	void		*mlx;
+	void		*window;
+	void		*image;
+	t_imagesize	windowsize;
+	t_imagesize	imagesize;
+	float		scalex;
+	float		scaley;
+	float		scale;
+	int			offsetx;
+	int			offsety;
+	int			bpp;
+	int			line_bytes;
+	int			endian;
+	char		*original_data;
+	void		*scaled_image;
+	int			color;
+	t_map		*map;
+}				t_drawdata;
 
-// Error utilities
-void free_draw_data(t_DrawData *drawData);
-void free_map(t_Map *map);
+// draw utilities
+void			put_pixel_to_image(void *image, int x, int y, int color);
+void			draw_line(t_linedata *linedata, t_drawdata *drawdata);
+int				close_window(t_drawdata *drawdata);
+int				key_event_handler(int keycode, t_drawdata *drawdata);
 
-// File utilities
-int open_file(const char *filepath);
-int count_integers_in_string(char *str);
+// error utilities
+void			free_draw_data(t_drawdata *drawdata);
+void			free_map(t_map *map);
 
-// Custom functions
-void *ft_realloc(void *ptr, size_t original_size, size_t new_size);
+// file utilities
+int				open_file(const char *filepath);
+int				count_integers_in_string(char *str);
 
-// Initialization fucntions
-void init_window_size(t_DrawData *drawData, t_Map *map);
-void init_image_size(t_DrawData *drawData, t_Map *map);
-int interpolate(int start, int end, float factor);
-int init_color(int z, int min_z, int max_z);
-void init_draw_data(t_DrawData *drawData);
+// custom functions
+void			*ft_realloc(void *ptr, size_t original_size, size_t new_size);
 
-// Map utilities
-int find_max_z_value(t_Map *map);
-int find_min_z_value(t_Map *map);
-int *string_to_int_array(char *str, int *outLength);
-void fill_map_lines(t_Map *map, char *line);
-int map_filling(t_Map *map, char *file_name);
-void print_map(t_Map *map);
+// initialization fucntions
+void			init_window_size(t_drawdata *drawdata);
+void			init_image_size(t_drawdata *drawdata);
+int				interpolate(int start, int end, float factor);
+int				init_color(int z, int min_z, int max_z);
+void			init_draw_data(t_drawdata *drawdata);
+void			init_window_size_vars(t_imagesize *size);
 
+// map utilities
+int				find_max_z_value(t_map *map);
+int				find_min_z_value(t_map *map);
+int				*string_to_int_array(char *str, int *outlength);
+void			fill_map_lines(t_map *map, char *line);
+int				map_filling(t_map *map, char *file_name);
+void			print_map(t_map *map);
 
 #endif

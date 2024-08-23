915e00eb9c1a254c50eb6ba8f6f15b6af3ae5c5b
szb <chamaselion@gmail.com>
2024-08-23 14:47:38 +0200

    Norminettize

--- fdf_init.c
+++ fdf_init.c
@@ -1,89 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_init.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 13:15:19 by bszikora          #+#    #+#             */
+/*   Updated: 2024/08/23 14:42:59 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-void init_window_size(t_DrawData *drawData, t_Map *map)
+void	init_window_size(t_drawdata *drawdata)
 {
-    int a;
-    
-    a = 175;
-  //  int b = 1300;
-    drawData->windowSize.x -= a * 0.5625;
-    drawData->windowSize.y -= a;
-    //drawData->windowSize.x = b;
-  //  drawData->windowSize.y = b;
+	int	a;
+
+	a = 175;
+	drawdata->windowsize.x -= a * 0.5625;
+	drawdata->windowsize.y -= a;
 }
 
-void init_image_size(t_DrawData *drawData, t_Map *map)
+void	init_image_size(t_drawdata *drawdata)
 {
-    drawData->imageSize.x = drawData->windowSize.x; 
-    drawData->imageSize.y = drawData->windowSize.y;
+	drawdata->imagesize.x = drawdata->windowsize.x;
+	drawdata->imagesize.y = drawdata->windowsize.y;
 }
 
-int interpolate(int start, int end, float factor)
+int	interpolate(int start, int end, float factor)
 {
-    return (int)(start + (end - start) * factor);
+	return ((int)(start + (end - start) * factor));
 }
 
-int init_color(int z, int min_z, int max_z)
+int	init_color(int z, int min_z, int max_z)
 {
-    int red;
-    int green;
-    int blue;
-    float normalized;
-    // Check if z is negative
-    if (z < 0) {
-        // Return green color
-        return (0 << 16) | (255 << 8) | 0;
-    }
+	int		red;
+	int		green;
+	int		blue;
+	float	normalized;
 
-    // Check if z is zero
-    if (z == 0) {
-        // Return white color
-        return (255 << 16) | (255 << 8) | 255;
-    }
-
-    // Normalize the height value to a range between 0 and 1
-    normalized = (float)(z - min_z) / (max_z - min_z);
-
-    // Map the normalized value to a color gradient from white to magenta
-    red = (int)(normalized * 255);
-    green = (int)((1 - normalized) * 255);
-    blue = 255;
-
-    return (red << 16) | (green << 8) | blue;
+	if (z < 0)
+	{
+		return ((0 << 16) | (255 << 8) | 0);
+	}
+	if (z == 0)
+	{
+		return ((255 << 16) | (255 << 8) | 255);
+	}
+	normalized = (float)(z - min_z) / (max_z - min_z);
+	red = (int)(normalized * 255);
+	green = (int)((1 - normalized) * 255);
+	blue = 255;
+	return ((red << 16) | (green << 8) | blue);
 }
 
-void init_window_size_vars(t_ImageSize *size)
+void	init_window_size_vars(t_imagesize *size)
 {
-    size->x = 0;
-    size->y = 0;
-    size->sx = 0;
-    size->sy = 0;
-}
-
-void init_image_size_vars(t_ImageSize *size)
-{
-    size->x = 0;
-    size->y = 0;
-    size->sx = 0;
-    size->sy = 0;
-}
-
-void init_draw_data(t_DrawData *drawData)
-{
-    drawData->mlx = NULL;
-    drawData->window = NULL;
-    drawData->image = NULL;
-    init_window_size_vars(&drawData->windowSize);
-    init_image_size_vars(&drawData->imageSize);
-    drawData->scaleX = 1.0;
-    drawData->scaleY = 1.0;
-    drawData->scale = 1.0f;
-    drawData->offsetX = 0;
-    drawData->offsetY = 0;
-    drawData->bpp = 0;
-    drawData->line_bytes = 0;
-    drawData->endian = 0;
-    drawData->original_data = NULL;
-    drawData->scaled_image = NULL;
-    drawData->color = 0xFFFFFF; // Default color (white)
+	size->x = 0;
+	size->y = 0;
+	size->sx = 0;
+	size->sy = 0;
 }

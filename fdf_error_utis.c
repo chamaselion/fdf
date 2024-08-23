add106f49f008c830b2c3c39bc5472ffbb6a0620
szb <chamaselion@gmail.com>
2024-08-23 14:46:27 +0200

    Norminettize

--- fdf_error_utis.c
+++ fdf_error_utis.c
@@ -1,56 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_error_utis.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 13:13:15 by bszikora          #+#    #+#             */
+/*   Updated: 2024/08/23 14:41:21 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-void free_draw_data(t_DrawData *drawData)
+void	free_n_null(void **ptr)
 {
-    if (drawData->image != NULL)
-    {
-        mlx_destroy_image(drawData->mlx, drawData->image);
-        drawData->image = NULL;
-    }
-    if (drawData->window != NULL)
-    {
-        mlx_destroy_window(drawData->mlx, drawData->window);
-        drawData->window = NULL;
-    }
-    if (drawData->mlx != NULL)
-    {
-        mlx_destroy_display(drawData->mlx);
-        free(drawData->mlx);
-        drawData->mlx = NULL;
-    }
-    if (drawData->original_data != NULL)
-    {
-        free(drawData->original_data);
-        drawData->original_data = NULL;
-    }
-    if (drawData->scaled_image != NULL)
-    {
-        free(drawData->scaled_image);
-        drawData->scaled_image = NULL;
-    }
+	if (*ptr != NULL)
+	{
+		free(*ptr);
+		*ptr = NULL;
+	}
 }
 
-void free_map(t_Map *map)
+void	free_draw_data(t_drawdata *drawdata)
 {
-    int i;
+	if (drawdata->image != NULL)
+	{
+		mlx_destroy_image(drawdata->mlx, drawdata->image);
+		drawdata->image = NULL;
+	}
+	if (drawdata->window != NULL)
+	{
+		mlx_destroy_window(drawdata->mlx, drawdata->window);
+		drawdata->window = NULL;
+	}
+	if (drawdata->mlx != NULL)
+	{
+		mlx_destroy_display(drawdata->mlx);
+		free(drawdata->mlx);
+		drawdata->mlx = NULL;
+	}
+	if (drawdata->original_data != NULL)
+		free_n_null((void **)&drawdata->original_data);
+	if (drawdata->scaled_image != NULL)
+		free_n_null((void **)&drawdata->scaled_image);
+}
 
-    i = 0;
-    if (map != NULL)
-    {
-        if (map->values != NULL)
-        {
-            while (i < map->height)
-            {
-                if (map->values[i] != NULL)
-                {
-                    free(map->values[i]);
-                    map->values[i] = NULL;
-                }
-                i++;
-            }
-            free(map->values);
-            map->values = NULL;
-        }
-        //free(map);
-    }
+void	free_map(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	if (map != NULL)
+	{
+		if (map->values != NULL)
+		{
+			while (i < map->height)
+			{
+				if (map->values[i] != NULL)
+				{
+					free(map->values[i]);
+					map->values[i] = NULL;
+				}
+				i++;
+			}
+			free(map->values);
+			map->values = NULL;
+		}
+	}
 }

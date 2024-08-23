795ede79fb925b64c2b70e5e9561f72c4ae7fa0c
szb <chamaselion@gmail.com>
2024-08-23 14:46:45 +0200

    Norminettize

--- fdf_file_utis.c
+++ fdf_file_utis.c
@@ -1,40 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_file_utis.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 14:26:02 by bszikora          #+#    #+#             */
+/*   Updated: 2024/08/23 14:41:47 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-int open_file(const char *filepath)
+int	open_file(const char *filepath)
 {
-    int fd;
-    fd = open(filepath, O_RDONLY);
-    return fd;
+	int	fd;
+
+	fd = open(filepath, O_RDONLY);
+	return (fd);
 }
 
-int count_integers_in_string(char *str)
+int	count_integers_in_string(char *str)
 {
-    int count;
-    int length;
-    int inNumber;
-    int c;
+	int	count;
+	int	length;
+	int	innumber;
+	int	c;
 
-    count = 0;
-    length = strlen(str);
-    inNumber = 0;
-    c = 0;
-
-    while (c <= length - 1)
-    {
-        if (!isspace(str[c]) && str[c] != '\0')
-        {
-            if (!inNumber)
-            {
-                inNumber = 1;
-                count++;
-            }
-        }
-        else
-        {
-            inNumber = 0;
-        }
-    c++;
-    }
-
-    return count;
+	count = 0;
+	length = strlen(str);
+	innumber = 0;
+	c = 0;
+	while (c <= length - 1)
+	{
+		if (!isspace(str[c]) && str[c] != '\0')
+		{
+			if (!innumber)
+			{
+				innumber = 1;
+				count++;
+			}
+		}
+		else
+			innumber = 0;
+		c++;
+	}
+	return (count);
 }

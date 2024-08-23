456a1a3ce4a9b2bd3c42dc3ee295df5e9660c215
szb <chamaselion@gmail.com>
2024-08-23 16:57:43 +0200

    normineette

--- fdf_draw_utis.c
+++ fdf_draw_utis.c
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 13:11:38 by bszikora          #+#    #+#             */
-/*   Updated: 2024/08/23 14:28:38 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/08/23 14:54:19 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,8 @@ void	put_pixel_to_image(void *image, int x, int y, int color)
 	*(int *)(buffer + pos) = color;
 }
 
-void	draw_line(t_linedata *linedata, t_drawdata *drawdata)
+void	linedata_decide(t_linedata *linedata)
 {
-	int	image_width;
-	int	image_height;
-
-	image_width = drawdata->windowsize.x;
-	image_height = drawdata->windowsize.y;
-	if (linedata->x0 < 0 || linedata->x0 >= image_width || linedata->y0 < 0
-		|| linedata->y0 >= image_height || linedata->x1 < 0
-		|| linedata->x1 >= image_width || linedata->y1 < 0
-		|| linedata->y1 >= image_height)
-	{
-		printf("coordinates out of bounds: (%d, %d) to (%d, %d)\n",
-			linedata->x0, linedata->y0, linedata->x1, linedata->y1);
-		return ;
-	}
-	linedata->dx = abs(linedata->x1 - linedata->x0);
-	linedata->dy = abs(linedata->y1 - linedata->y0);
 	if (linedata->x0 < linedata->x1)
 		linedata->sx = 1;
 	else
@@ -55,7 +39,10 @@ void	draw_line(t_linedata *linedata, t_drawdata *drawdata)
 		linedata->err = linedata->dx;
 	else
 		linedata->err = -linedata->dy;
-	linedata->err /= 2;
+}
+
+void	line_reversesniffer(t_linedata *linedata, t_drawdata *drawdata)
+{
 	while (1)
 	{
 		put_pixel_to_image(drawdata->image, linedata->x0, linedata->y0,
@@ -76,6 +63,29 @@ void	draw_line(t_linedata *linedata, t_drawdata *drawdata)
 	}
 }
 
+void	draw_line(t_linedata *linedata, t_drawdata *drawdata)
+{
+	int	image_width;
+	int	image_height;
+
+	image_width = drawdata->windowsize.x;
+	image_height = drawdata->windowsize.y;
+	if (linedata->x0 < 0 || linedata->x0 >= image_width || linedata->y0 < 0
+		|| linedata->y0 >= image_height || linedata->x1 < 0
+		|| linedata->x1 >= image_width || linedata->y1 < 0
+		|| linedata->y1 >= image_height)
+	{
+		printf("coordinates out of bounds: (%d, %d) to (%d, %d)\n",
+			linedata->x0, linedata->y0, linedata->x1, linedata->y1);
+		return ;
+	}
+	linedata->dx = abs(linedata->x1 - linedata->x0);
+	linedata->dy = abs(linedata->y1 - linedata->y0);
+	linedata_decide(linedata);
+	linedata->err /= 2;
+	line_reversesniffer(linedata, drawdata);
+}
+
 int	close_window(t_drawdata *drawdata)
 {
 	if (drawdata->window != NULL)
@@ -86,21 +96,5 @@ int	close_window(t_drawdata *drawdata)
 	free_draw_data(drawdata);
 	free_map(drawdata->map);
 	exit(0);
-	return (0);
-}
-
-int	key_event_handler(int keycode, t_drawdata *drawdata)
-{
-	if (keycode == 65307)
-	{
-		if (drawdata->window != NULL)
-		{
-			mlx_destroy_window(drawdata->mlx, drawdata->window);
-			drawdata->window = NULL;
-		}
-		free_draw_data(drawdata);
-		free_map(drawdata->map);
-		exit(0);
-	}
 	return (0);
 }
